8e1b86320c35a9807675bba6eea80bab0ccb1935
SandraAlcaraz <sandra_alcaraz.a@outlook.com>
2019-05-10 21:28:56 -0500

    put

--- src/main/java/com/sandra/service/task/TaskController.java
+++ src/main/java/com/sandra/service/task/TaskController.java
@@ -35,10 +35,13 @@ public class TaskController {
     }
 
     @GetMapping(path = "/tasks/user")
-    public String getTasksForUser( @RequestHeader("userId") Integer userId){
+    public @ResponseBody List<Task> getTasksForUser( @RequestHeader("userId") Integer userId){
         //Task t= repository.findByUser(userId);
-
-        return "Get all tasks for an user";
+       List<Task> tasks=  repository.findByUserId(userId);
+       if(tasks.isEmpty()){
+           throw new UserNotFoundException();
+       }
+       return tasks;
     }
 
     @GetMapping(path = "/tasks/user/{taskId}")
@@ -53,18 +56,39 @@ public class TaskController {
     }
 
     @DeleteMapping(path = "/tasks/user/{taskId}")
-    public String deleteTaskForUser(@PathVariable Integer taskId){
-        return "Delete specific user task {"+taskId+"}";
+    public @ResponseBody Task deleteTaskForUser(@PathVariable Integer taskId,  @RequestHeader("userId") Integer userId){
+        Task t= repository.findByIdAndUserId(taskId,userId );
+        if(t==null){
+            throw new TaskNotFoundException();
+        }
+        repository.delete(t);
+        return t;
     }
 
     @PutMapping(path = "/tasks/user/{taskId}")
-    public String updateTaskForUser(@PathVariable Integer taskId){
+    public Task updateTaskForUser(@PathVariable Integer taskId,
+                                  @RequestHeader("userId") Integer userId,
+                                  @RequestBody Task task){
+        Task t= repository.findByIdAndUserId(taskId,userId );
 
-        return "Updates specific user task {"+taskId+"}";
+        if(t==null){
+            throw new TaskNotFoundException();
+        }
+        t.setId(taskId);
+        t.setDescription(task.getDescription());
+        t.setDifficulty(task.getDifficulty());
+        t.setDueDate(task.getDueDate());
+        t.setReminder(task.getReminder());
+        t.setTitle(task.getTitle());
+        repository.save(t);
+        return t;
     }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Task or user not found")
     public  class TaskNotFoundException extends RuntimeException{
+    }
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found")
+    public  class UserNotFoundException extends RuntimeException{
     }
 
 }
--- src/main/java/com/sandra/service/task/TaskRepository.java
+++ src/main/java/com/sandra/service/task/TaskRepository.java
@@ -2,9 +2,11 @@ package com.sandra.service.task;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends CrudRepository<Task, Integer> {
 
     public Task findByIdAndUserId(Integer id, Integer userId);
-
+    public List<Task> findByUserId(Integer userId);
 
 }
